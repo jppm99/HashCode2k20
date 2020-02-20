d2c6f19e33e5873092280f0763e1eb693c8c58c0
Diogo Machado <diogo.smac@gmail.com>
2020-02-20 18:29:13 +0000

    Basic value extraction

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-name = ''
+name = 'a_example'
 # name of the file to be processed
 
 
 class Solver(object):
 
     def __init__(self, filename):
+        self.TotalBooks = 0
+        self.TotalLibraries = 0
+        self.TotalDays = 0
+        self.bookScores = []            # access by book index
+        self.LibraryBookCount = []      # access by library index
+        self.LibrarySignupTime = []     # access by library index
+        self.LibraryDebitPerDay = []    # access by library index
+        self.LibraryBookList = []       # [n][m] -> n is library index, m is book index (inside library)
         self.filename = filename
         self.get_input()
 
@@ -12,9 +20,23 @@ class Solver(object):
         return 0
 
     def get_input(self):
-        return 0
-
+        path = 'input/' + self.filename + '.txt'
+        with open(path, 'r') as f:
+            [self.TotalBooks, self.TotalLibraries, self.TotalDays] = [int(el) for el in f.readline().split()]
+            self.bookScores = [int(el) for el in f.readline().split()]
+            for i in range(self.TotalLibraries):
+                [bookCount, signup, debit] = [int(el) for el in f.readline().split()]
+                self.LibraryBookCount.append(bookCount)
+                self.LibrarySignupTime.append(signup)
+                self.LibraryDebitPerDay.append(debit)
+                self.LibraryBookList.append([int(el) for el in f.readline().split()])
+            
     def write(self):
+        print(str(self.TotalBooks), str(self.TotalLibraries), str(self.TotalDays))
+        print(self.bookScores)
+        for i in range(self.TotalLibraries):
+            print(str(self.LibraryBookCount[i]), str(self.LibrarySignupTime[i]), str(self.LibraryDebitPerDay[i]))
+            print(self.LibraryBookList[i])
         return 0
 
     def solve(self):
@@ -31,8 +53,8 @@ def main():
     solver = Solver(name)
     solver.get_input()
 
-    solver.solve()
-    print('Total Score:', solver.total_score())
+    # solver.solve()
+    # print('Total Score:', solver.total_score())
 
     solver.write()
 
