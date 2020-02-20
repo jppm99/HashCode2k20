1a538597d7cf79d63990cdc999b3aa9e6f997018
Diogo Machado <diogo.smac@gmail.com>
2020-02-20 18:36:31 +0000

    Added library class (this.organization)

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-name = 'a_example'
+name = 'a_example.txt'
 # name of the file to be processed
 
+def printArrayInLine(array):
+    line = ''
+    for i in array:
+        line += str(i) + ' '
+    print(line)
+
+class Library(object):
+
+    def __init__(self, count, signup, debit, books):
+        self.BookCount = count
+        self.SignupTime = signup
+        self.DebitPerDay = debit
+        self.BookList = books
+
+    def write(self):
+        print(self.BookCount, self.SignupTime, self.DebitPerDay)
+        printArrayInLine(self.BookList)
 
 class Solver(object):
 
@@ -8,11 +25,8 @@ class Solver(object):
         self.TotalBooks = 0
         self.TotalLibraries = 0
         self.TotalDays = 0
-        self.bookScores = []            # access by book index
-        self.LibraryBookCount = []      # access by library index
-        self.LibrarySignupTime = []     # access by library index
-        self.LibraryDebitPerDay = []    # access by library index
-        self.LibraryBookList = []       # [n][m] -> n is library index, m is book index (inside library)
+        self.BookScores = []            # access by book index
+        self.Libraries = []             # access by library index (duh)
         self.filename = filename
         self.get_input()
 
@@ -20,23 +34,21 @@ class Solver(object):
         return 0
 
     def get_input(self):
-        path = 'input/' + self.filename + '.txt'
+        path = 'input/' + self.filename
         with open(path, 'r') as f:
             [self.TotalBooks, self.TotalLibraries, self.TotalDays] = [int(el) for el in f.readline().split()]
-            self.bookScores = [int(el) for el in f.readline().split()]
+            self.BookScores = [int(el) for el in f.readline().split()]
             for i in range(self.TotalLibraries):
-                [bookCount, signup, debit] = [int(el) for el in f.readline().split()]
-                self.LibraryBookCount.append(bookCount)
-                self.LibrarySignupTime.append(signup)
-                self.LibraryDebitPerDay.append(debit)
-                self.LibraryBookList.append([int(el) for el in f.readline().split()])
+                [count, signup, debit] = [int(el) for el in f.readline().split()]
+                books = [int(el) for el in f.readline().split()]
+                lib = Library(count, signup, debit, books)
+                self.Libraries.append(lib)
             
     def write(self):
         print(str(self.TotalBooks), str(self.TotalLibraries), str(self.TotalDays))
-        print(self.bookScores)
-        for i in range(self.TotalLibraries):
-            print(str(self.LibraryBookCount[i]), str(self.LibrarySignupTime[i]), str(self.LibraryDebitPerDay[i]))
-            print(self.LibraryBookList[i])
+        printArrayInLine(self.BookScores)
+        for lib in self.Libraries:
+            lib.write()
         return 0
 
     def solve(self):
